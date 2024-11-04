c6b9c0f622a3198ad90bdf6c83eac3888ecd634c
Jackson G <jagman00@gmail.com>
2024-11-04 12:46:36 -0500

    adding content to files

---
+++ vault.js
@@ -0,0 +1,18 @@
+// This script will add functionality to the vault html page
+// First will be creating strings to store the vault code
+// Each vault code will the calculated using different arithmetic equations
+// Next will be to create the alert that displays the vault code to the site visitor
+
+
+// The vault code will not change so using constants here to set the code to the vault
+const code1 = 5 + 5;
+const code2 = 80 / 2;
+const code3 = 60 * 2 - 81;
+
+// This first alert it to notify the page viewer that they have a secret mission
+alert("You have recieved this message because you have been chosen to open an important vault.");
+
+// This second alert is to give the site visitor the code to the vault
+alert(`Here is the secret combination: ${code1}-${code2}-${code3}`);
+
+
